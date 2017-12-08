081ac44b0542cef256809cea6e242ae3baa5231b
van.duong <nhat-tuong-van.duong@etu.u-pec.fr>
2017-12-08 14:49:55 +0100

    Add TA for OpenStatementFromList US and Cucumber dependencies

---
+++ app/src/androidTest/java/esipe/fisa/silber/OpenStatementFromListTest.java
@@ -0,0 +1,39 @@
+package esipe.fisa.silber;
+
+/**
+ * Created by vanessaduong on 08/12/2017.
+ */
+
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class OpenStatementFromListTest {
+
+    @Given("^the application is started$")
+    public void the_application_is_started() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^the user click on the name of the november statement$")
+    public void the_user_click_on_the_name_of_the_november_statement() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^the choosen account statement is opened with a PDF reader$")
+    public void the_choosen_account_statement_is_opened_with_a_pdf_reader() throws Throwable {
+        throw new PendingException();
+    }
+
+    @And("^the application list 5 latest statements november, october, september, august and july 2017  $")
+    public void the_application_list_5_latest_statements_november_october_september_august_and_july_2017() throws Throwable {
+        throw new PendingException();
+    }
+
+}
